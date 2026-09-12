cf589debc108b17fe45327f45cae4a7c93ea61a2
wolgranf@gmail.com <wolgranf@gmail.com>
2026-09-12 16:16:20 -0300

    Exercicio fatiamento_cont

--- fatiamento_string.py
+++ fatiamento_string.py
@@ -30,19 +30,22 @@ Se nada for digitado em nome ou idade:
 nome = input("Digite seu nome: ")
 idade = input("Digite sua idade: ")
 
+""" Se o usuario não digitar retorna (False) e cai no else"""
+if nome and idade: 
+    print(f"Seu nome é:{nome}")
+    print(f"Seu nome invertido é: {nome[::-1]}")
+    print(f"Seu nome contém {nome.count(' ')} espaços")
+    print(f"Seu nome tem {len(nome.replace(' ', ''))} letras")
+    print(f" A primeira letra do seu nome é: {nome[0]}")
+    print(f" A ultima letra do seu nome é: {nome[-1]}")
+else:
+    print("Desculpe, você deixou campos vazios")
+    
 
-print(f"Seu nome é:{nome}")
-print(f"Seu nome invertido é: {nome[::-1]}")
-print(f"Seu nome contém {nome.count(' ')} espaços")
 if ' ' in nome:
     print("Seu nome contém espaços.")
 else:
     print("Seu nome não contem espaços.")
-print(f"Seu nome tem {len(nome.replace(' ', ''))} letras")
-print(f" A primeira letra do seu nome é: {nome[0]}")
-print(f" A ultima letra do seu nome é: {nome[-1]}")
-if not nome.strip() or not idade.strip():
-    print("Desculpe, você deixou campos vazios")
-else:
-   idade = int(idade) 
+
+
 
